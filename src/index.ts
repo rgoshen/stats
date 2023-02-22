db30cc2e3b4863ab1d3627087bf0326d056513b8
Richard Goshen <rick.goshen@gmail.com>
2023-02-21 20:22:14 -0700

    add read in the csv file and parsed the data

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('hello world!');
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', 'utf-8')
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+console.log(matches);
