1a2621134e9bdbe3cce5d382ab84793de031c6e1
Richard Goshen <rick.goshen@gmail.com>
2023-02-23 16:46:55 -0700

    wip - refactor csvFileReader to make it reusable again

---
+++ build/MatchReader.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MatchReader = void 0;
+const CsvFileReader_1 = require("./CsvFileReader");
+class MatchReader extends CsvFileReader_1.CsvFileReader {
+}
+exports.MatchReader = MatchReader;
+(row[0]),
+    row[1],
+    row[2],
+    parseInt(row[3]),
+    parseInt(row[4]),
+    row[5],
+    row[6],
+;
+;
+;
