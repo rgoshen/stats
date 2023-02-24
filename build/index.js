3b8af91ac6c16d8da88213cc054ce27fb65b925e
Richard Goshen <rick.goshen@gmail.com>
2023-02-23 21:47:36 -0700

    refactor (alternate) CsvFileReader.ts to accept any csv file, create DataReader interface and MatchReader.ts to load and format from csv file read in by CsvFileReader.ts

--- build/index.js
+++ build/index.js
@@ -1,11 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const MatchResult_1 = require("./MatchResult");
+const CsvFileReader_1 = require("./CsvFileReader");
 const MatchReader_1 = require("./MatchReader");
-const reader = new MatchReader_1.MatchReader('football.csv');
-reader.read();
+// Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
+// Create an instance of 'MatchReader' and pass in something satisfying
+// the 'DataReader' interface
+const matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
 let manUnitedWins = 0;
-for (const match of reader.data) {
+for (const match of matchReader.matches) {
     if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.homeWin) {
         manUnitedWins++;
     }
@@ -13,4 +18,4 @@ for (const match of reader.data) {
         manUnitedWins++;
     }
 }
-console.log(`Man United won ${manUnitedWins} games.`);
+console.log(`Man United won ${manUnitedWins} games`);
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import { MatchResult } from './MatchResult';
+import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+// Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv');
+
+// Create an instance of 'MatchReader' and pass in something satisfying
+// the 'DataReader' interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0;
 
-for (const match of reader.data) {
+for (const match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.homeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.awayWin) {
@@ -14,4 +20,4 @@ for (const match of reader.data) {
   }
 }
 
-console.log(`Man United won ${manUnitedWins} games.`);
+console.log(`Man United won ${manUnitedWins} games`);
